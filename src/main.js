3ae1b2ed36bd7ed6fc8fe157f23a331ffef22bd0
Henriquedearaujo <ricke_cris@hotmail.com>
2024-03-17 19:29:33 -0300

    question

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll("[data-tab-button]");
+  const question = document.querySelectorAll("[data-faq-question]");
+  
 
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function (botao) {
@@ -11,7 +13,18 @@ document.addEventListener("DOMContentLoaded", function () {
       botao.target.classList.add('shows__tabs__button--is-active')
     });
   }
+
+  for (let i = 0; i < question.length; i++) {
+    question[i].addEventListener('click', abreOuFechaResposta);
+  }
 });
+
+function abreOuFechaResposta(elemento) {
+  const classe = 'faq__questions__item--is-open';
+  const elementoPai = elemento.target.parentNode;
+
+  elementoPai.classList.toggle(classe)
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
